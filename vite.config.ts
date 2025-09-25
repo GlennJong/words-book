fcb69b581eb35ee8ba0ccfc5f8bf39509667b8e0
GlennJong <zaku0225@gmail.com>
2025-09-25 15:09:57 +0000

    Modify development config for demo mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      }
-  },
+export default defineConfig(({ mode }) => {
+  const isDemo = mode === 'demo';
+  return {
+    base: './',
+    plugins: [react()],
+    resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        }
+    },
+    build: {
+      outDir: isDemo ? 'dist/demo' : 'dist'
+    },
+    define: {
+      'import.meta.env.VITE_IS_DEMO': JSON.stringify(isDemo)
+    }
+  }
 })
